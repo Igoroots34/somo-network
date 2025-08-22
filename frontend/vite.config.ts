268ed5b064a5982e5d50c22e800902d0018737c6
Igoroots34 <igorgdf34@gmail.com>
2025-08-22 14:01:48 -0300

    config vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
   },
   define: {
-    'process.env.WS_URL': JSON.stringify(process.env.WS_URL || 'ws://localhost:8000/ws'),
+    'process.env.WS_URL': JSON.stringify(process.env.WS_URL || 'ws://somo-network-backend.onrender.com/ws'),
   }
 })
 
